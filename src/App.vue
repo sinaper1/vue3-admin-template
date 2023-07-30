5fecf7c0ca075cf4dc0bbe035717abe2e8e3182e
xiufulong <xiufulong@que360.com>
2023-07-30 23:21:29 +0800

    初始化代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>我是APP</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 600px;
+  background: #000;
+
+  h1 {
+    color: #fff;
+  }
+}
+</style>
